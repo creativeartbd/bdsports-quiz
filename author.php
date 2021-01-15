5917a8cea9fe7e0440e0596ea31615a676ea8b1c
creativeartbd <creativeartbd@gmail.com>
2021-01-15 14:02:11 +0600

    Fix author page

--- author.php
+++ author.php
@@ -15,16 +15,7 @@ do_action('hestia_before_single_post_wrapper');
 <div class="<?php echo hestia_layout(); ?>">
     <div class="blog-post blog-post-wrapper">
         <div class="container">
-            <?php
-            if (have_posts()) :
-                while (have_posts()) :
-                    the_post();
-                    get_template_part('template-parts/content', 'author');
-                endwhile;
-            else :
-                get_template_part('template-parts/content', 'none');
-            endif;
-            ?>
+            <?php get_template_part('template-parts/content', 'author'); ?>
         </div>
     </div>
 </div>
